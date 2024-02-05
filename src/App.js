7fdb3e2ac85a390ddbbe575a7bd7426905cdee41
pushapraj <pushap.raj@venturepact.com>
2024-02-05 17:03:25 +0530

    edit btn

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
+  const [editIndex, setEditIndex] = useState(null);
+  const [editedTodo, setEditedTodo] = useState("");
 
   const addTodo = () => {
     if (todo !== "") {
@@ -15,23 +17,50 @@ const App = () => {
   };
 
   const deleteTodo = (text) => {
-    const newTodos = todos.filter((todo) => {
-      return todo !== text;
-    });
+    const newTodos = todos.filter((t) => t !== text);
     setTodos(newTodos);
+    setEditIndex(null);
+    setEditedTodo("");
+  };
+
+  const editTodo = (index) => {
+    setEditIndex(index);
+    setEditedTodo(todos[index]);
+  };
+
+  const updateTodo = () => {
+    const updatedTodos = [...todos];
+    updatedTodos[editIndex] = editedTodo;
+    setTodos(updatedTodos);
+    setEditIndex(null);
+    setEditedTodo("");
   };
   const handleClearAll = () => {
     setTodos([]);
+    setEditIndex(null);
+    setEditedTodo("");
   };
+  const cancelEdit = () => {
+    setEditIndex(null);
+    setEditedTodo('');
+  };
+
   return (
     <div className="App">
-      {/* <img src="../logo192.png" alt="logo"/> */}
       <h1>Todo App</h1>
       <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
-      <TodoList list={todos} remove={deleteTodo} />
-      {todos.length > 1 && ( <button onClick={handleClearAll}>Clear All</button> )}
-      
-
+      <TodoList
+        list={todos}
+        remove={deleteTodo}
+        editTodo={editTodo}
+        editIndex={editIndex}
+        setEditedTodo={setEditedTodo}
+        updateTodo={updateTodo}
+        cancelEdit={cancelEdit}
+      />
+      {todos.length > 1 && (
+        <button onClick={handleClearAll}>Clear All</button>
+      )}
     </div>
   );
 };
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,35 +1,51 @@
-const TodoList = ({ list, remove }) => {
+const TodoList = ({ list, remove, editTodo, editedTodo, cancelEdit, editIndex, setEditedTodo, updateTodo, }) => {
 
   return (
-    <>
-      {list?.length > 0 ? (
-        <ul className="todo-list">
-          {list.map((entry, index) => (
-            <div className="todo">
-              <li key={index}> {entry} </li>
-
-              <button
-                className="delete-button"
-                onClick={() => {
-                  remove(entry);
-                }}
-              >
-                Delete
-              </button>
-
+    <ul>
+      {list.map((todo, index) => (
+        <li key={index}>
+          {index === editIndex ? (
+          
+            <div className="edit-popup">
+            <div className="edit-popup_inr">
+              <input
+                type="text"
+                placeholder="...Update Task"
+                value={editedTodo}
+                onChange={(e) => setEditedTodo(e.target.value)}
+              />
+              <button onClick={() => updateTodo()}>Update</button>
+              <button onClick={() => cancelEdit()}>Cancel</button>
+              </div></div>
+            
+          ) : (
+            <div className="rgt_btn">
+              <div className="task_nm">
+                {todo}
+              </div>
+              <div className="btn">
+                <button onClick={() => editTodo(index)}>
+                <svg class="feather feather-edit" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24">
+                  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
+                  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
+                  </button>
+                  <button onClick={() => remove(todo)}>
+                    <svg enable-background="new 0 0 48 48" height="24px" version="1.1" viewBox="0 0 48 48" width="24px">
+                      <g id="Expanded"><g><g><path d="M41,48H7V7h34V48z M9,46h30V9H9V46z"/></g><g><path d="M35,9H13V1h22V9z M15,7h18V3H15V7z"/></g>
+                      <g><path d="M16,41c-0.553,0-1-0.447-1-1V15c0-0.553,0.447-1,1-1s1,0.447,1,1v25C17,40.553,16.553,41,16,41z"/></g>
+                      <g><path d="M24,41c-0.553,0-1-0.447-1-1V15c0-0.553,0.447-1,1-1s1,0.447,1,1v25C25,40.553,24.553,41,24,41z"/></g>
+                      <g><path d="M32,41c-0.553,0-1-0.447-1-1V15c0-0.553,0.447-1,1-1s1,0.447,1,1v25C33,40.553,32.553,41,32,41z"/></g>
+                      <g><rect height="2" width="48" y="7"/></g></g></g></svg>
+                    </button>
+                </div>
             </div>
-          ))}
-        </ul>
-      ) : (
-        <div className="empty">
-          <p>No task found</p>
-        </div>
-      )}
-
-
-      
-    </>
+          )}
+        </li>
+      ))}
+    </ul>
   );
 };
 
 export default TodoList;
+
+
